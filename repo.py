7f26690b4fd6ec026bfde2ffbe5985ea2dc55910
Soumya Modagi <soumyamodagi1@gmail.com>
2026-01-18 14:34:15 +0530

    abc

--- repo.py
+++ repo.py
@@ -3,3 +3,9 @@ b = float(input("Enter second number: "))
 
 print(f"Addition: {a + b}")
 print(f"Subtraction: {a - b}")
+print(f"Multiplication: {a * b}")
+
+if b != 0:
+    print(f"Division: {a / b}")
+else:
+    print("Division: Cannot divide by zero")
